60c69572ba87454b0898a5092299ae084034c05d
Andy Xia <31865985+AndyXia-6@users.noreply.github.com>
2022-10-26 23:08:54 -0600

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -3,8 +3,19 @@ import pytest
 from app import load_wiki_summary
 from app import answer_question
 
+def test_load_wiki_summary_query(x):
+    testResults = wikipedia.search("Calgary")
+    testSummary = wikipedia.summary(results[0], sentences=10)
+    return testSummary
+    
+def test_load_wiki_summary:   
+     results = wikipedia.search("Calgary")
+     summary = wikipedia.summary(results[0], sentences=10)    
+    
+     assert test_load_wiki_summary_query(x)=summary
+    
 def func(x):
     return x + 1
-
+    
 def test_answer():
     assert func(3) == 4
