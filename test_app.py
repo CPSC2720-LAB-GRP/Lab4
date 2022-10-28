0b4435ed7022a24cc1a10b6b66b6028ce38dc77f
KaramveerSinghSidhu <65443742+KaramveerSinghSidhu@users.noreply.github.com>
2022-10-27 20:39:12 -0600

    Update and rename pytest.py to test_app.py

---
+++ test_app.py
@@ -0,0 +1,23 @@
+import pytest
+import wikipedia
+import streamlit as st
+from tokenizers import Tokenizer
+from transformers import pipeline, Pipeline 
+
+
+# def test_load_wiki_summary_query(x):
+#     testResults = wikipedia.search("Calgary")
+#     testSummary = wikipedia.summary(results[0], sentences=10)
+#     return testSummary
+    
+# def test_load_wiki_summary():   
+#      results = wikipedia.search("Calgary")
+#      summary = wikipedia.summary(results[0], sentences=10)    
+    
+#      assert test_load_wiki_summary_query(x)=summary
+    
+def func(x):
+    return x + 1
+    
+def test_answer():
+    assert func(3) == 4
